4a541cebc393c2bc618a9bc9c3a65c40163f0f9a
ahmedabdelhafez <messi10010@gmail.com>
2026-03-19 20:10:32 +0200

    refactor: Add deprecated re-export stubs for singular country dialog components.

---
+++ src/features/countries/components/country-action-dialog.tsx
@@ -0,0 +1,2 @@
+// This file is deprecated. Use countries-action-dialog.tsx instead.
+export { CountriesActionDialog } from './countries-action-dialog'
---
+++ src/features/countries/components/country-delete-dialog.tsx
@@ -0,0 +1,2 @@
+// This file is deprecated. Use countries-delete-dialog.tsx instead.
+export { CountriesDeleteDialog } from './countries-delete-dialog'
